994c9f1559b0bfaf62cc8f98aefed4f7082399ff
Empresa <empresa@aulas.local>
2018-06-14 11:31:53 +0200

    tabla pruebas readme

---
+++ test/readme.md
@@ -0,0 +1,12 @@
+# Tabla de Tests
+
+## Resultados
+
+| Entrada | Salida | Descipcion |
+| ------- | ------ | ---------- |
+|   "()"  |    0   | Ha de devolver 0 que indica que la función está balanceada |
+| 	"(()" |	   1   | Ha de devolver 1 que significa que hay un parétesis abierto que no está cerrado previo a uno que sí está correctamente cerrado |
+|	")()" |	  -1   | Ha de devolver -1 que significa que hay un paréntesis cerrado que no ha sido abierto previo a uno que sí está correctamente cerrado |
+|   "()(" |    1   | Ha de devolver 1 que significa que hay un parétesis abierto que no está cerrado después de uno que sí está correctamente cerrado |
+|   "())" |   -1   | Ha de devolver -1 que significa que hay un parétesis abierto que no está cerrado después de uno que sí está correctamente cerrado |
+
---
+++ test/testExpresiones.php
@@ -0,0 +1,38 @@
+<?php
+	use PHPUnit\Framework\TestCase;
+	use lib\utils\Expresiones;
+	require_once "src\Expresiones.php";
+	
+	class testExpresiones extends TestCase{
+		
+		public function testCadenaBalanceada(){
+			$cad = "()";
+			$result = Expresiones::secuenciaParentesisBalanceada($cad);
+			$this->assertequals(0, $result);
+		}
+
+		public function testCadenaNoBalanceadaAperturaIzq(){
+			$cad = "(()";
+			$result = Expresiones::secuenciaParentesisBalanceada($cad);
+			$this->assertequals(1, $result);
+		}
+
+		public function testCadenaNoBalanceadaCierreIzq(){
+			$cad = ")()";
+			$result = Expresiones::secuenciaParentesisBalanceada($cad);
+			$this->assertequals(-1, $result);
+		}
+
+		public function testCadenaNoBalanceadaAperturaDer(){
+			$cad = "()(";
+			$result = Expresiones::secuenciaParentesisBalanceada($cad);
+			$this->assertequals(1, $result);
+		}
+
+		public function testCadenaNoBalanceadaCierreDer(){
+			$cad = "())";
+			$result = Expresiones::secuenciaParentesisBalanceada($cad);
+			$this->assertequals(-1, $result);
+		}
+
+?>
